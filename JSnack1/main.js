257775a22ead9dbf873e8ab41d52e569a21ce7c4
ValentinaNovarino <valenovarino@gmail.com>
2020-11-02 14:50:31 +0100

    jsnack1 creato array e stampato

---
+++ JSnack1/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+    // creare l'array di oggetti
+    var bikeModels = [
+        {
+            'name': 'Wilier 0 SLR',
+            'weight': 6
+        },
+        {
+            'name': 'Cannondale SuperSix EVOHi-MOD Disc Dura Ace',
+            'weight': 8
+        },
+        {
+            'name': 'Trek Emonda 2020',
+            'weight': 7
+        },
+        {
+            'name': 'SAVA Phantom 2.0 700C',
+            'weight': 8
+        }
+    ];
+
+    for (var i = 0; i < bikeModels.length; i++) {
+        console.log("Bicicletta " + (i+1));
+        for(var key in bikeModels[i]){
+            console.log(key + ": " + bikeModels[i][key]);
+        }
+    };
+});
