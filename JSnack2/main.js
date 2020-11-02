6da7c2d1e36fe86489229ca4ce49d1b57fda5b1c
ValentinaNovarino <valenovarino@gmail.com>
2020-11-02 19:45:14 +0100

    jsnack2 array nomi e numeri utente

---
+++ JSnack2/main.js
@@ -0,0 +1,26 @@
+// JSnack2: Creare un array di nomi e chiedere all'utente due numeri. Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.
+
+$(document).ready(function() {
+    // creo un array di nomi
+    var names = ['Mario', 'Maria', 'Anna', 'Francesca', 'Luigi', 'Valerio', 'Giacomo'];
+    // console.log(names);
+    // creare un array vuoto dove inserire i numeri dell'numeri_utente
+    var arrayUserNumbers = [];
+
+    // chiedere all'utente due numeri
+    while(arrayUserNumbers.length < 2) {
+
+        var userNumber = parseInt(prompt("Inserisci un numero tra 0 e 6 compresi"));
+
+        if(!isNaN(userNumber) && userNumber >= 0 && userNumber <= 6){
+            arrayUserNumbers.push(userNumber);
+        }else{
+            alert("Non hai inserio un valore valido");
+        }
+    }
+    console.log(arrayUserNumbers);
+
+    // // creo un array vuoto dove inserire i nomi compresi nelle posizioni scelte dall'utente
+    // var userName = [];
+
+});
